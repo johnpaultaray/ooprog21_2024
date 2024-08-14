d2ecc5932c816ff51b6400f2acc8b068c1ad8713
tarayjohn1 <tarayjohn1@gmail.com>
2024-08-14 23:32:48 +0800

    Add Exercise A to C

---
+++ Chapter 1/Exercise A/MovieQuoteInfo.java
@@ -0,0 +1,8 @@
+class MovieQuoteInfo {
+   public static void main(String[] args) {
+      System.out.println("After all, tomorrow is another day! ");
+      System.out.println("said by Scarlett O'Hara ");
+      System.out.println("in the movie Gone with the Wind ");
+      System.out.println("in 1939.");
+   }
+}
